28df3f8a757f8abb73773256ddf39333a14d19fb
sakozo <youlianggit@gmail.com>
2019-12-13 06:49:00 +0900

    messages#createが動くようにする

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,18 @@
 $(function(){
   $('#new_message').on('submit',function(e){
     e.preventDefault();
-    console.log('発火！！');
-  })
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    console.log(url);
+    console.log(formData);
+    console.log(this);
+    $.ajax({
+      url: url,
+      type: 'POST',
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    });
+  });
 });

8e595d9f5757d7fb3068b570b7faa619b2e8865e
sakozo <youlianggit@gmail.com>
2019-12-14 15:37:26 +0900

    非同期通信実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,11 +1,32 @@
 $(function(){
+  function buildHTML(message){
+    if (message.image) {
+      var html = `
+      <div class="message">
+        <div class="message__message-info">
+          <p class="message__message-info__name">${message.name}</p>
+          <p class="message__message-info__date">${message.date}</p>
+        </div>
+          <p class="message__message-text">${message.body}</p>
+          <img class="message__message-img" src=${message.image}>
+      </div>`
+    } else {
+      var html = `
+      <div class="message">
+        <div class="message__message-info">
+          <p class="message__message-info__name">${message.name}</p>
+          <p class="message__message-info__date">${message.date}</p>
+        </div>
+          <p class="message__message-text">${message.body}</p>
+      </div>`
+    }
+    return html
+  }
+
   $('#new_message').on('submit',function(e){
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action');
-    console.log(url);
-    console.log(formData);
-    console.log(this);
     $.ajax({
       url: url,
       type: 'POST',
@@ -13,6 +34,18 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
-    });
+    })
+    .done(function(data){
+      var messages = $('.messages');
+      var html = buildHTML(data);
+      messages.append(html);
+      messages.animate({ scrollTop: $('.messages')[0].scrollHeight});
+      $('#new_message')[0].reset();
+      $('.form__submit-button').prop('disabled', false);
+    })
+    .fail(function(){
+      alert("メッセージを入力してください");
+      $('.form__submit-button').prop('disabled', false);
+    })
   });
 });
---
+++ app/views/messages/create.json.jbuilder
@@ -0,0 +1,4 @@
+json.body  @message.body
+json.name  @message.user.name
+json.date  @message.created_at.strftime("%Y/%m/%d %H:%M")
+json.image @message.image.url

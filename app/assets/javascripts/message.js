053afe67f0bde10f4f07fb28fb91033181f4207f
sakozo <youlianggit@gmail.com>
2019-12-16 23:23:23 +0900

    自動更新実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,9 @@
 $(function(){
+  
   function buildHTML(message){
     if (message.image) {
       var html = `
-      <div class="message">
+      <div class="message" data-message-id=${message.id}>
         <div class="message__message-info">
           <p class="message__message-info__name">${message.name}</p>
           <p class="message__message-info__date">${message.date}</p>
@@ -12,7 +13,7 @@ $(function(){
       </div>`
     } else {
       var html = `
-      <div class="message">
+      <div class="message" data-message-id=${message.id}>
         <div class="message__message-info">
           <p class="message__message-info__name">${message.name}</p>
           <p class="message__message-info__date">${message.date}</p>
@@ -48,4 +49,45 @@ $(function(){
       $('.form__submit-button').prop('disabled', false);
     })
   });
+
+  if(window.location.href.match(/\/groups\/\d+\/messages/)){
+    var reloadMessages = function() {
+      last_message_id = $('.message:last').data('message-id');
+      console.log(last_message_id);
+
+      //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+      last_message_id = $('.message:last').data('message-id');
+      $.ajax({
+        //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+        url: "api/messages",
+        //ルーティングで設定した通りhttpメソッドをgetに指定
+        type: 'get',
+        dataType: 'json',
+        //dataオプションでリクエストに値を含める
+        data: {id: last_message_id}
+      })
+      .done(function(messages) {
+        //追加するHTMLの入れ物を作る
+        console.log(messages);
+        var insertHTML = '';
+        //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        //メッセージが入ったHTMLに、入れ物ごと追加
+        $('.messages').append(insertHTML);
+        console.log(insertHTML);
+        console.log('success');
+        if(insertHTML.length > 0){
+          console.log(insertHTML.length)
+          $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+        }
+      })
+      .fail(function(messages) {
+        console.log('error');
+        console.log(messages)
+      });
+    };
+    setInterval(reloadMessages, 7000);
+  }
 });

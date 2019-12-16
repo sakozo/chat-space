66fa64ac033ca4e7ee1b0b3d0a920f951196377d
sakozo <youlianggit@gmail.com>
2019-12-17 06:38:45 +0900

    不要な記述を削除

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -58,34 +58,23 @@ $(function(){
       //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
       last_message_id = $('.message:last').data('message-id');
       $.ajax({
-        //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
         url: "api/messages",
-        //ルーティングで設定した通りhttpメソッドをgetに指定
         type: 'get',
         dataType: 'json',
-        //dataオプションでリクエストに値を含める
         data: {id: last_message_id}
       })
       .done(function(messages) {
-        //追加するHTMLの入れ物を作る
-        console.log(messages);
         var insertHTML = '';
-        //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
         $.each(messages, function(i, message) {
           insertHTML += buildHTML(message)
         });
-        //メッセージが入ったHTMLに、入れ物ごと追加
         $('.messages').append(insertHTML);
-        console.log(insertHTML);
-        console.log('success');
         if(insertHTML.length > 0){
-          console.log(insertHTML.length)
           $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
         }
       })
-      .fail(function(messages) {
-        console.log('error');
-        console.log(messages)
+      .fail(function() {
+        alert("自動更新に失敗しました");
       });
     };
     setInterval(reloadMessages, 7000);
